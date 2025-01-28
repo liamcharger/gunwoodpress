a8362548469c9d3c5f19b6d981114cfdb047036a
liamcharger <chargerelectronics19@gmail.com>
2025-01-28 16:31:18 -0500

    Initialize Firebase and update styles and copy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import styles from "./styles/App.module.css";
 import logo from "./assets/logoHorizontal.png"
-// import logoStacked from "./assets/logoStacked.png"
+import logoStacked from "./assets/logoStacked.png"
 
 function App() {
   return (
     <div className={styles.app}>
     <header>
-    {/* <img src={logoStacked} alt="Logo"></img> */}
+    <img src={logoStacked} alt="Logo"></img>
     <div className={styles.spacer}></div>
     {/* A href causes a page refresh; we may want to use a button */}
     <a href="/books" className={styles.btnSecondary}>Our Books</a>
@@ -16,7 +16,7 @@ function App() {
     <img src={logo} alt="Logo"></img>
     {/* <h1>Gunwood Press</h1> */}
     <div className={styles.text}>
-    Gunwood press is a majorly big publishing house, and needs YOUR MONEY!!! Lorem ipsum odor amet, consectetuer adipiscing elit. Volutpat etiam suspendisse nascetur imperdiet himenaeos consequat. Efficitur leo habitant ipsum nam ultricies facilisi. Vestibulum convallis pretium vehicula donec fermentum dis.
+    Publisher description should go here...Lorem ipsum odor amet, consectetuer adipiscing elit. Volutpat etiam suspendisse nascetur imperdiet himenaeos consequat. Efficitur leo habitant ipsum nam ultricies facilisi. Vestibulum convallis pretium vehicula donec fermentum dis.
     </div>
     </div>
     <footer>© 2025 Gunwood Press — Made with ❤️ by Liam Willey</footer>
